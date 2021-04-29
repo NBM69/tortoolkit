2b1c9a68ebcbe119aad755d93ddfb81694b3d62e
reaitten <riojosepha@gmail.com>
2021-04-28 22:04:00 -0400

    replaced & fixed

---
+++ tortoolkit/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__version__ = "0.2.4"
+__author__ = "YashDK Github@yash-dk"
+
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
+    handlers=[logging.StreamHandler(),logging.FileHandler("torlog.txt")]
+)
+
+from tortoolkit.core.wserver import start_server
+from .core.database_handle import TtkUpload,TorToolkitDB,TtkTorrents, UserDB
+from .core.varholdern import VarHolder
+import time
+
+logging.info("Database created")
+upload_db = TtkUpload()
+var_db = TorToolkitDB()
+tor_db = TtkTorrents()
+user_db = UserDB()
+
+uptime = time.time()
+to_del = []
+SessionVars = VarHolder(var_db)
